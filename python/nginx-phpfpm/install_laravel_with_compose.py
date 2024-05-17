04972b1031fe0dfaab14e6d25d4bdc87b94a09e7
anti1346 <anti1346@mt.co.kr>
2024-05-17 16:40:38 +0900

    no message

--- python/nginx-phpfpm/install_laravel_with_compose.py
+++ python/nginx-phpfpm/install_laravel_with_compose.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 
 # 실행 결과를 출력하는 함수
@@ -16,26 +15,16 @@ def install_packages(packages):
     for package in packages:
         run_command(f"sudo apt-get install -y {package}")
 
-# PHP 버전 변수 정의
-php_version = "8.1"
-
-# 필수 패키지 설치
-required_packages = [f"php{php_version}-intl"]
+# 업데이트 및 필수 패키지 설치
+run_command("sudo apt-get update")
+required_packages = [f"php8.1-intl", "composer"]
 install_packages(required_packages)
 
-# 업데이트
-run_command("sudo apt-get update")
-
 # PHP-FPM 서비스 재시작
-run_command(f"sudo systemctl restart php{php_version}-fpm")
-
-# composer 설치
-composer_install_command = "sudo apt-get install -y composer"
-subprocess.run(composer_install_command, shell=True)
+run_command("sudo systemctl restart php8.1-fpm")
 
 # Laravel 설치
-laravel_install_command = "composer global require laravel/installer"
-subprocess.run(laravel_install_command, shell=True)
+run_command("composer global require laravel/installer")
 
 # Nginx 설정 파일 업데이트
 nginx_conf_default_content = """
@@ -98,14 +87,13 @@ print("Configuration file '/etc/nginx/conf.d/default.conf' created.")
 laravel_project_path = "/usr/share/nginx/html"
 laravel_project_name = "laravel_project"
 laravel_create_command = f"cd {laravel_project_path} && composer create-project --prefer-dist laravel/laravel {laravel_project_name}"
-subprocess.run(laravel_create_command, shell=True)
+run_command(laravel_create_command)
 
 # Laravel 프로젝트 디렉토리 권한 설정
 laravel_chmod_command = f"sudo chown -R www-data:www-data {laravel_project_path}/{laravel_project_name}"
-subprocess.run(laravel_chmod_command, shell=True)
+run_command(laravel_chmod_command)
 
 # Nginx 서비스 재시작
-nginx_restart_command = "sudo systemctl restart nginx"
-subprocess.run(nginx_restart_command, shell=True)
+run_command("sudo systemctl restart nginx")
 
 print("Laravel 설치 및 연동이 완료되었습니다.")
