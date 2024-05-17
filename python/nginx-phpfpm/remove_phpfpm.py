101e153b8a64e1ab7618cfe6884366e3c9ccde9f
anti1346 <anti1346@mt.co.kr>
2024-05-17 13:16:16 +0900

    no message

--- python/nginx-phpfpm/remove_phpfpm.py
+++ python/nginx-phpfpm/remove_phpfpm.py
@@ -1,11 +1,14 @@
 import subprocess
 
+# PHP 버전 변수 정의
+php_version = "8.1"
+
 # 명령어 리스트
 commands = [
     # PHP-FPM 서비스 중지
-    "sudo systemctl stop php-fpm",
+    "sudo systemctl status php{php_version}-fpm",
     # PHP-FPM 서비스 비활성화
-    "sudo systemctl disable php-fpm",
+    "sudo systemctl disable php{php_version}-fpm",
     # PHP 관련 패키지 제거
     "sudo apt-get purge -y php-cli php-common php-composer-ca-bundle php-composer-metadata-minifier php-composer-pcre php-composer-semver php-composer-spdx-licenses php-composer-xdebug-handler php-intl php-json-schema php-mbstring php-psr-container php-psr-log php-react-promise php-symfony-console php-symfony-deprecation-contracts php-symfony-filesystem php-symfony-finder php-symfony-polyfill-php80 php-symfony-process php-symfony-service-contracts php-symfony-string php8.1-cli php8.1-common php8.1-intl php8.1-mbstring php8.1-opcache php8.1-readline",
     # 사용하지 않는 패키지 자동 제거
