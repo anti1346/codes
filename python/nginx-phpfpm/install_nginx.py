fe2d1f0c1c3342378b5deb86f5047912497ecb45
anti1346 <anti1346@mt.co.kr>
2024-05-17 12:43:12 +0900

    no message

--- python/nginx-phpfpm/install_nginx.py
+++ python/nginx-phpfpm/install_nginx.py
@@ -1,24 +1,26 @@
 import subprocess
 
 def run_command(command):
-    # subprocess.run()으로 명령어 실행하고 stdout, stderr를 분리하여 출력합니다.
+    """명령어를 실행하고 결과를 출력합니다."""
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     if result.returncode != 0:
-        # 명령어 실행 실패 시 에러 메시지 출력하고 종료합니다.
         print(f"Command failed: {command}")
         print(result.stderr)
         exit(1)
     else:
-        # 명령어 실행 성공 시 표준 출력을 출력합니다.
         print(result.stdout)
+
+def install_packages(packages):
+    """주어진 패키지들을 설치합니다."""
+    for package in packages:
+        run_command(f"sudo apt-get install -y {package}")
 
 # 업데이트
 run_command("sudo apt-get update")
 
 # 필수 패키지 설치
 required_packages = ["curl", "gnupg2", "ca-certificates", "lsb-release", "ubuntu-keyring", "apt-transport-https"]
-for package in required_packages:
-    run_command(f"sudo apt-get install -y {package}")
+install_packages(required_packages)
 
 # NGINX 키 설치
 run_command("curl -s https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null")
@@ -35,7 +37,7 @@ run_command(nginx_repo_command)
 run_command("sudo apt-get update")
 
 # NGINX 설치
-run_command("sudo apt-get install -y nginx")
+install_packages(["nginx"])
 
 # NGINX 버전 확인
 run_command("nginx -v")
@@ -46,50 +48,64 @@ run_command("sudo systemctl --now enable nginx")
 # NGINX 상태 확인
 run_command("sudo systemctl status nginx")
 
-# nginx.conf 설정 추가
-nginx_conf_path = "/etc/nginx/nginx.conf"
-with open(nginx_conf_path, 'r') as file:
-    nginx_conf = file.readlines()
+# NGINX 설정 추가
+nginx_conf_content = """
+user  www-data;
+worker_processes  auto;
 
-http_index = next(i for i, line in enumerate(nginx_conf) if line.strip() == "http {")
-server_block = """
+error_log  /var/log/nginx/error.log notice;
+pid        /var/run/nginx.pid;
+
+events {
+    worker_connections  1024;
+}
+
+http {
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
+
+    access_log  /var/log/nginx/access.log  main;
+
     server_tokens off;
-"""
-nginx_conf.insert(http_index + 1, server_block)
 
-with open(nginx_conf_path, 'w') as file:
-    file.writelines(nginx_conf)
+    sendfile        on;
+    keepalive_timeout  65;
+
+    include /etc/nginx/conf.d/*.conf;
+}
+"""
+with open('/etc/nginx/nginx.conf', 'w') as file:
+    file.write(nginx_conf_content)
+print("Configuration file '/etc/nginx/nginx.conf' created.")
 
 # default.conf 설정 추가
-nginx_conf_default_path = "/etc/nginx/conf.d/default.conf"
-with open(nginx_conf_default_path, 'r') as file:
-    nginx_conf_default = file.readlines()
-
-http_index = next(i for i, line in enumerate(nginx_conf_default) if line.strip() == "http {")
-server_block = """
-    server {
-        listen       80;
-        server_name  _;
-        location / {
-            root   /usr/share/nginx/html;
-            index  index.html index.htm;
-        }
-        location /nginx_status {
-            stub_status;
-            access_log off;
-            allow 127.0.0.1;
-            allow 192.168.56.0/24;
-            deny all;
-        }
-        location ~ /\.ht {
-            deny  all;
-        }
+nginx_conf_default_content = """
+server {
+    listen       80;
+    server_name  _;
+    location / {
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
     }
+    location /nginx_status {
+        stub_status;
+        access_log off;
+        allow 127.0.0.1;
+        allow 192.168.56.0/24;
+        deny all;
+    }
+    location ~ /\.ht {
+        deny  all;
+    }
+}
 """
-nginx_conf_default.insert(http_index + 1, server_block)
-
-with open(nginx_conf_default_path, 'w') as file:
-    file.writelines(nginx_conf_default)
+with open('/etc/nginx/conf.d/default.conf', 'w') as file:
+    file.write(nginx_conf_default_content)
+print("Configuration file '/etc/nginx/conf.d/default.conf' created.")
 
 # NGINX 설정 테스트
 run_command("nginx -t")
