433a28c76517c35e9ea6ce745d1625402841d317
anti1346 <anti1346@mt.co.kr>
2024-05-17 13:18:21 +0900

    no message

--- python/nginx-phpfpm/remove_phpfpm.py
+++ python/nginx-phpfpm/remove_phpfpm.py
@@ -5,12 +5,11 @@ php_version = "8.1"
 
 # 명령어 리스트
 commands = [
-    # PHP-FPM 서비스 중지
-    "sudo systemctl status php{php_version}-fpm",
-    # PHP-FPM 서비스 비활성화
-    "sudo systemctl disable php{php_version}-fpm",
+    # PHP-FPM 서비스 중지 및 비활성화
+    f"sudo systemctl stop php{php_version}-fpm",
+    f"sudo systemctl disable php{php_version}-fpm",
     # PHP 관련 패키지 제거
-    "sudo apt-get purge -y php-cli php-common php-composer-ca-bundle php-composer-metadata-minifier php-composer-pcre php-composer-semver php-composer-spdx-licenses php-composer-xdebug-handler php-intl php-json-schema php-mbstring php-psr-container php-psr-log php-react-promise php-symfony-console php-symfony-deprecation-contracts php-symfony-filesystem php-symfony-finder php-symfony-polyfill-php80 php-symfony-process php-symfony-service-contracts php-symfony-string php8.1-cli php8.1-common php8.1-intl php8.1-mbstring php8.1-opcache php8.1-readline",
+    f"sudo apt-get purge -y php-cli php-common php-composer-ca-bundle php-composer-metadata-minifier php-composer-pcre php-composer-semver php-composer-spdx-licenses php-composer-xdebug-handler php-intl php-json-schema php-mbstring php-psr-container php-psr-log php-react-promise php-symfony-console php-symfony-deprecation-contracts php-symfony-filesystem php-symfony-finder php-symfony-polyfill-php80 php-symfony-process php-symfony-service-contracts php-symfony-string php{php_version}-cli php{php_version}-common php{php_version}-intl php{php_version}-mbstring php{php_version}-opcache php{php_version}-readline",
     # 사용하지 않는 패키지 자동 제거
     "sudo apt-get autoremove -y",
     # 설정 파일 및 로그 파일 삭제
@@ -29,4 +28,3 @@ for command in commands:
     except subprocess.CalledProcessError as e:
         # 실행 오류 메시지 출력
         print(f"Error executing command '{command}': {e}")
-
