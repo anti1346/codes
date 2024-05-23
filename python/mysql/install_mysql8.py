e9ee42b755bb12939cf57d09b6253361ff4dbaa3
anti1346 <anti1346@mt.co.kr>
2024-05-23 14:42:09 +0900

    no message

--- python/mysql/install_mysql8.py
+++ python/mysql/install_mysql8.py
@@ -14,57 +14,10 @@ MYSQL_INSTALL_DIR = "/usr/local/mysql"
 def run_command(command):
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     if result.returncode != 0:
-        print(f"Error executing command: {command}")
+        print(f"Error executing commnad: {command}")
         print(result.stderr)
     return result
 
-# MySQL 사용자 생성
+#MySQL 사용자 생성
 def create_mysql_user():
-    if run_command("id mysql").returncode != 0:
-        if run_command("getent group mysql").returncode != 0:
-            run_command("sudo groupadd -r mysql")
-        run_command("sudo useradd -M -N -g mysql -o -r -d {} -s /bin/false -c 'MySQL Server' -u 27 mysql".format(MYSQL_INSTALL_DIR))
-
-# 필수 라이브러리 설치
-def install_libraries():
-    if run_command("command -v apt-get").returncode == 0:
-        run_command("sudo apt-get update")
-        run_command("sudo apt-get install -y libncurses5 libaio1 libnuma1")
-    elif run_command("command -v yum").returncode == 0:
-        run_command("sudo yum install -y ncurses-compat-libs libaio numactl")
-    else:
-        print("Unsupported package manager.")
-        exit(1)
-
-# MySQL 패키지 다운로드 및 설치
-def download_and_install_mysql():
-    mysql_package_path = Path(WORK_DIR) / MYSQL_PACKAGE
-    if not mysql_package_path.is_file():
-        urllib.request.urlretrieve(f"{MYSQL_DOWNLOAD_URL}/{MYSQL_PACKAGE}", mysql_package_path)
-
-    os.makedirs(f"{MYSQL_INSTALL_DIR}/data", exist_ok=True)
-    run_command(f"sudo tar xf {mysql_package_path} -C {MYSQL_INSTALL_DIR} --strip-components=1")
-    run_command(f"sudo chown -R mysql:mysql {MYSQL_INSTALL_DIR}")
-
-# MySQL 환경 변수 등록
-def setup_mysql_environment():
-    bashrc_path = Path.home() / ".bashrc"
-    with open(bashrc_path, "a") as bashrc:
-        if f"{MYSQL_INSTALL_DIR}/bin" not in bashrc.read():
-            bashrc.write(f"\nexport PATH={MYSQL_INSTALL_DIR}/bin:$PATH\n")
-
-# MySQL 버전 확인
-def check_mysql_version():
-    result = run_command(f"{MYSQL_INSTALL_DIR}/bin/mysqld -V")
-    print("\nMySQL Version\n---")
-    print(result.stdout)
-
-def main():
-    create_mysql_user()
-    install_libraries()
-    download_and_install_mysql()
-    setup_mysql_environment()
-    check_mysql_version()
-
-if __name__ == "__main__":
-    main()
+    if run_command(id mysql").ret
