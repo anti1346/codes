d68c62422fa0e6901313d4afa7a81e598eaddcb3
anti1346 <anti1346@mt.co.kr>
2024-05-24 11:12:54 +0900

    no message

--- python/mysql/install_mysql8.py
+++ python/mysql/install_mysql8.py
@@ -127,6 +127,7 @@ def start_mysql():
     result = run_command(command)
     if result.returncode == 0:
         print("MySQL server started successfully.")
+        exit(0)
     else:
         print("Failed to start MySQL server.")
 
