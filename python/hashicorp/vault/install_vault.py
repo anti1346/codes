109982f4e616b0df6823889f279c0b5db1bb335f
anti1346 <anti1346@mt.co.kr>
2024-05-24 15:02:26 +0900

    no message

--- python/hashicorp/vault/install_vault.py
+++ python/hashicorp/vault/install_vault.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-import datetime
 
 def run_command(command, check=True):
     """명령어를 실행하고 결과를 출력합니다."""
@@ -18,18 +17,21 @@ def install_packages(packages):
         run_command(f"sudo apt-get install -y {package}")
 
 def install_vault():
-    install_packages(["gpg", "lsb-release"])
+    # 필요한 패키지 설치
+    install_packages(["gpg", "lsb-release", "curl"])
+    # HashiCorp GPG 키 추가
     run_command("curl -s https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg >/dev/null")
-
+    # 배포판 코드네임 가져오기
     lsb_release = run_command("lsb_release -cs")
+    # HashiCorp APT 리포지토리 추가
     run_command(f'echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com {lsb_release} main" | sudo tee /etc/apt/sources.list.d/hashicorp.list')
-
+    # Vault 설치
     install_packages(["vault"])
 
 def check_vault_version():
-    result = run_command(f"vault --version")
-    print("\nVault Version\n---")
-    print(result.stdout)
+    """Vault 버전을 확인합니다."""
+    result = run_command("vault --version")
+    print(result)
 
 if __name__ == "__main__":
     install_vault()
