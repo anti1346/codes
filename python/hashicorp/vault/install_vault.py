66f7ac9f03fad7a66ccb999590e6ee703a36c606
anti1346 <anti1346@mt.co.kr>
2024-05-24 14:49:40 +0900

    no message

--- python/hashicorp/vault/install_vault.py
+++ python/hashicorp/vault/install_vault.py
@@ -18,12 +18,15 @@ def install_packages(packages):
         run_command(f"sudo apt-get install -y {package}")
 
 def install_vault():
-    install_packages(gpg)
+    install_packages(["gpg"])
     run_command("curl -s https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg >/dev/null")
 
     lsb_release = run_command("lsb_release -cs")
-    run_command = (f"'echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com {lsb_release} main" | sudo tee /etc/apt/sources.list.d/hashicorp.list'")
-    
+    run_command(f'echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com {lsb_release} main" | sudo tee /etc/apt/sources.list.d/hashicorp.list')
+
     install_packages(["vault"])
+
+if __name__ == "__main__":
+    install_vault()
 
 
