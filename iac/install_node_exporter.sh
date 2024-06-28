b423d4f0e8fe67ecf2f18f4541254c5ef8648611
anti1346 <anti1346@mt.co.kr>
2024-06-28 13:48:20 +0900

    no message

--- iac/install_node_exporter.sh
+++ iac/install_node_exporter.sh
@@ -17,43 +17,40 @@ else
     exit 1
 fi
 
-echo $OS
+# Create node_exporter user
+if ! id -u node_exporter &>/dev/null; then
+    useradd -r -m -s /usr/sbin/nologin node_exporter
+fi
 
-# # Create node_exporter user
-# if ! id -u node_exporter &>/dev/null; then
-#     useradd -r -m -s /usr/sbin/nologin node_exporter
-# fi
+# Download and install node_exporter
+cd /usr/local/src
+wget -q https://github.com/prometheus/node_exporter/releases/download/v${Version}/node_exporter-${Version}.${OS}-${Arch}.tar.gz
+tar xfz node_exporter-${Version}.${OS}-${Arch}.tar.gz
+cp node_exporter-${Version}.${OS}-${Arch}/node_exporter /usr/local/bin/
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
-# # Download and install node_exporter
-# cd /usr/local/src
-# wget -q https://github.com/prometheus/node_exporter/releases/download/v${Version}/node_exporter-${Version}.${OS}-${Arch}.tar.gz
-# tar xfz node_exporter-${Version}.${OS}-${Arch}.tar.gz
-# cp node_exporter-${Version}.${OS}-${Arch}/node_exporter /usr/local/bin/
-# chown node_exporter:node_exporter /usr/local/bin/node_exporter
+# Create systemd service file
+cat << 'EOF' > /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Prometheus Node Exporter
+Wants=network-online.target
+After=network-online.target
 
-# # Create systemd service file
-# cat << 'EOF' > /etc/systemd/system/node_exporter.service
-# [Unit]
-# Description=Prometheus Node Exporter
-# Wants=network-online.target
-# After=network-online.target
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
 
-# [Service]
-# User=node_exporter
-# Group=node_exporter
-# Type=simple
-# ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
+EOF
 
-# [Install]
-# WantedBy=multi-user.target
-# EOF
+# Reload systemd and enable service
+systemctl daemon-reload
+systemctl enable node_exporter --now
 
-# # Reload systemd and enable service
-# systemctl daemon-reload
-# systemctl enable node_exporter --now
-
-# echo "Node Exporter is running. Access metrics at: http://localhost:9100/metrics"
-
+echo "Node Exporter is running. Access metrics at: http://localhost:9100/metrics"
 
 
 
