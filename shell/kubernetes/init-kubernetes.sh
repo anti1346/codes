22ad842aa35427a57f86850498052a211e38778e
aby <anti1346@gmail.com>
2024-07-16 00:34:42 +0900

    no message

--- shell/kubernetes/init-kubernetes.sh
+++ shell/kubernetes/init-kubernetes.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-sudo kubeadm config images pull
+sudo kubeadm config images pull --cri-socket unix:///var/run/containerd/containerd.sock
 
-sudo kubeadm init --pod-network-cidr 10.244.0.0/16 --cri-socket /run/containerd/containerd.sock
+sudo kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address=192.168.56.111 --cri-socket unix:///var/run/containerd/containerd.sock
 
 
+##### Verify #####
+kubeadm certs check-expiration
 
+
+# Install the bash-completion framework
+sudo apt-get install -y bash-completion
+# Output bash completion
+sudo sh -c 'kubeadm completion bash > /etc/bash_completion.d/kubeadm'
+sudo sh -c 'kubectl completion bash > /etc/bash_completion.d/kubectl'
+sudo sh -c 'crictl completion > /etc/bash_completion.d/crictl'
+# Load the completion code for bash into the current shell
+source /etc/bash_completion
 
 ### Kubernetes Documentation - Ports and Protocols : https://kubernetes.io/docs/reference/networking/ports-and-protocols/
