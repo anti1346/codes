84a3383c7e30b1fd81a4ff8eec585df6e9b7275f
anti1346 <anti1346@mt.co.kr>
2024-08-09 14:56:13 +0900

    no message

---
+++ kubernetes/kubeadm-config-v1.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -e
+
+kubeadm init phase certs ca
+kubeadm init phase certs apiserver
+kubeadm init phase certs apiserver-kubelet-client
+kubeadm init phase certs front-proxy-ca
+kubeadm init phase certs front-proxy-client
+kubeadm init phase certs etcd-ca
+kubeadm init phase certs etcd-server
+kubeadm init phase certs etcd-peer
+kubeadm init phase certs etcd-healthcheck-client
+kubeadm init phase certs apiserver-etcd-client
+kubeadm init phase certs sa
+
+cat <<EOF | sudo tee kubeadm-config.yaml
+apiVersion: kubeadm.k8s.io/v1beta3
+kind: InitConfiguration
+localAPIEndpoint:
+  advertiseAddress: "192.168.10.111"
+  bindPort: 6443
+nodeRegistration:
+  name: "node111"
+  kubeletExtraArgs:
+    node-labels: "node-role.kubernetes.io/master="
+  criSocket: "/var/run/containerd/containerd.sock"
+---
+apiVersion: kubeadm.k8s.io/v1beta3
+kind: ClusterConfiguration
+clusterName: "kubernetes"
+kubernetesVersion: "v1.27.0"
+controlPlaneEndpoint: "192.168.10.111:6443"
+apiServer:
+  certSANs:
+  - "192.168.10.111"
+  - "192.168.10.112"
+  - "192.168.10.113"
+  - "node111"
+  - "node112"
+  - "node113"
+etcd:
+  external:
+    endpoints:
+    - https://192.168.10.111:2379
+    - https://192.168.10.112:2379
+    - https://192.168.10.113:2379
+    caFile: /etc/kubernetes/pki/etcd/ca.crt
+    certFile: /etc/kubernetes/pki/etcd/healthcheck-client.crt
+    keyFile: /etc/kubernetes/pki/etcd/healthcheck-client.key
+networking:
+  podSubnet: "10.244.0.0/16"
+EOF
+
+kubeadm init --config kubeadm-config.yaml
+
+
+
+# curl -fsSL https://raw.githubusercontent.com/anti1346/codes/main/kubernetes/kubeadm-config.sh -o kubeadm-config.sh
+# chmod +x kubeadm-config.sh
+
--- kubernetes/kubeadm-config.sh
+++ kubernetes/kubeadm-config.sh
@@ -1,58 +1,47 @@
 #!/bin/bash
 
-set -e
+# 환경 변수 설정
+MASTER_NODE_IP="192.168.0.131"
+LOAD_BALANCER_DNS="192.168.0.130"
+ETCD_NODE1_IP="192.168.0.131"
+ETCD_NODE2_IP="192.168.0.132"
+ETCD_NODE3_IP="192.168.0.111"
 
-kubeadm init phase certs ca
-kubeadm init phase certs apiserver
-kubeadm init phase certs apiserver-kubelet-client
-kubeadm init phase certs front-proxy-ca
-kubeadm init phase certs front-proxy-client
-kubeadm init phase certs etcd-ca
-kubeadm init phase certs etcd-server
-kubeadm init phase certs etcd-peer
-kubeadm init phase certs etcd-healthcheck-client
-kubeadm init phase certs apiserver-etcd-client
-kubeadm init phase certs sa
-
-cat <<EOF | sudo tee kubeadm-config.yaml
+# kubeadm 설정 파일 생성
+cat << EOF > kubeadmcfg.yaml
+---
 apiVersion: kubeadm.k8s.io/v1beta3
 kind: InitConfiguration
 localAPIEndpoint:
-  advertiseAddress: "192.168.10.111"
+  advertiseAddress: ${MASTER_NODE_IP}
   bindPort: 6443
-nodeRegistration:
-  name: "node111"
-  kubeletExtraArgs:
-    node-labels: "node-role.kubernetes.io/master="
-  criSocket: "/var/run/containerd/containerd.sock"
+bootstrapTokens:
+- groups:
+  - system:bootstrappers:kubeadm:default-node-token
+  token: abcdef.0123456789abcdef
+  ttl: 24h0m0s
+  usages:
+  - signing
+  - authentication
 ---
 apiVersion: kubeadm.k8s.io/v1beta3
 kind: ClusterConfiguration
-clusterName: "kubernetes"
-kubernetesVersion: "v1.27.0"
-controlPlaneEndpoint: "192.168.10.111:6443"
-apiServer:
-  certSANs:
-  - "192.168.10.111"
-  - "192.168.10.112"
-  - "192.168.10.113"
-  - "node111"
-  - "node112"
-  - "node113"
+kubernetesVersion: 1.27.0
+controlPlaneEndpoint: "${LOAD_BALANCER_DNS}:2379"
+networking:
+  dnsDomain: cluster.local
+  serviceSubnet: 10.96.0.0/12
+  podSubnet: 192.168.0.0/16
 etcd:
   external:
     endpoints:
-    - https://192.168.10.111:2379
-    - https://192.168.10.112:2379
-    - https://192.168.10.113:2379
+    - https://${ETCD_NODE1_IP}:2379
+    - https://${ETCD_NODE2_IP}:2379
+    - https://${ETCD_NODE3_IP}:2379
     caFile: /etc/kubernetes/pki/etcd/ca.crt
-    certFile: /etc/kubernetes/pki/etcd/healthcheck-client.crt
-    keyFile: /etc/kubernetes/pki/etcd/healthcheck-client.key
-networking:
-  podSubnet: "10.244.0.0/16"
+    certFile: /etc/kubernetes/pki/etcd/peer.crt
+    keyFile: /etc/kubernetes/pki/etcd/peer.key
 EOF
-
-kubeadm init --config kubeadm-config.yaml
 
 
 
